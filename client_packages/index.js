459d6d2e5c4adec735edc1e60d65aa24a1ec381b
rchl2 <freshieboyz+git@gmail.com>
2018-06-06 21:08:13 +0200

    MapEditor

--- client_packages/index.js
+++
@@ -1,8 +0,0 @@
-require("./LSOnline/Util/Browser");
-require("./LSOnline/Util/Camera");
-require("./LSOnline/Notification/Notification");
-require("./LSOnline/Login/Login");
-require("./LSOnline/Util/Teleport");
-
-require('nativeui');
-require('charcreator');
