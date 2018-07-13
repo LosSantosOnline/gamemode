5308e1aa30aff7a9baa857d2162e93f05b1cf070
kubeeek <jakub.galecki5@gmail.com>
2018-07-13 04:17:29 +0200

    updated paths to views

--- client_packages/LSOnline/Notification/Notification.js
+++ client_packages/LSOnline/Notification/Notification.js
@@ -5,34 +5,33 @@ const browser = require("/LSOnline/Util/Browser");
 let notification = false;
 
 function showNotification(title, content) {
-    browser.open("package://LSOnline/Browsers/Notification/index.html");
-    browser.inject(`showNotification("${title}", "${content}");`);
-    notification = true;
-    mp.game.audio.playSoundFrontend(-1, "CHALLENGE_UNLOCKED", "HUD_AWARDS", true);
+  browser.open("package://LSOnline/Browsers/dist/notification/index.html");
+  browser.inject(`showNotification("${title}", "${content}");`);
+  notification = true;
+  mp.game.audio.playSoundFrontend(-1, "CHALLENGE_UNLOCKED", "HUD_AWARDS", true);
 }
 
-function destroyBrowser()
-{
-    browser.close();
-    notification = false;
+function destroyBrowser() {
+  browser.close();
+  notification = false;
 }
 
 function hideNotification() {
-    browser.inject(`hideNotification();`);
-    setTimeout(destroyBrowser, 1500);
+  browser.inject(`hideNotification();`);
+  setTimeout(destroyBrowser, 1500);
 }
 
 function notify(title, content, timeout = 3500) {
-    if (!notification) {
-        showNotification(title, content);
-        setTimeout(hideNotification, timeout);
-    }
+  if (!notification) {
+    showNotification(title, content);
+    setTimeout(hideNotification, timeout);
+  }
 }
 
 exports.notify = notify;
 
 mp.events.add({
-    'actionDone': (title, content, timeout = 3500) => {
-        notify(title, content, timeout);
-    }
+  actionDone: (title, content, timeout = 3500) => {
+    notify(title, content, timeout);
+  }
 });
--- packages/LSOnline/modules/auth/Login.js
+++ packages/LSOnline/modules/auth/Login.js
@@ -1,22 +1,26 @@
-"use strict";
+'use strict'
 
-function showLoginPanel(player) {
-    player.call(`loginPanelAppeared`, ["package://LSOnline/Browsers/Login/index.html"]);
+function showLoginPanel (player) {
+  player.call(`loginPanelAppeared`, [
+    'package://LSOnline/Browsers/dist/login/index.html'
+  ])
 }
 
 mp.events.add({
-    "playerJoin": async (player) => {
-        player.outputChatBox("Witaj na serwerze Los Santos Online!");
-        player.outputChatBox("Poczekaj chwilę, aktualnie pobierane są wszystkie zasoby serwera.");
-    },
-    "playerReady": async (player) => {
-        showLoginPanel(player);
-    },
-    "loginPlayer": async (player, password) => {
-        player.call(`actionDone`, [
-            `Witaj na serwerze!`,
-            "Zalogowałeś się po raz pierwszy na naszym serwerze. Wciśnij przycisk <b>Home</b>, " +
-            "aby wyświetlić dashboard konta wraz z panelem pomocy."
-        ]);
-    }
-});
+  playerJoin: async player => {
+    player.outputChatBox('Witaj na serwerze Los Santos Online!')
+    player.outputChatBox(
+      'Poczekaj chwilę, aktualnie pobierane są wszystkie zasoby serwera.'
+    )
+  },
+  playerReady: async player => {
+    showLoginPanel(player)
+  },
+  loginPlayer: async (player, password) => {
+    player.call(`actionDone`, [
+      `Witaj na serwerze!`,
+      'Zalogowałeś się po raz pierwszy na naszym serwerze. Wciśnij przycisk <b>Home</b>, ' +
+        'aby wyświetlić dashboard konta wraz z panelem pomocy.'
+    ])
+  }
+})
