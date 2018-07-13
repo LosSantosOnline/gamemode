1bafd8d3874f7ee3e4069256ceb199fb4d38ccae
kubeeek <jakub.galecki5@gmail.com>
2018-07-13 16:47:40 +0200

    Webpack (#5)
    * updated .gitignore file for webpack
    
    * Added instructions with npm scripts to README file
    
    * Added webpack to project
    
    * Removed old files
    
    * Rewritten login view to vue component
    
    * Rewritten notification view to vue component
    
    * Added webpack template file for views
    
    * Added missing background image
    
    * fixed typo
    
    * updated paths to views
    
    * final corrections
    
    * updated package.json for webpack bundling
    
    * final corrections
    
    * updated package.json for webpack bundling

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Los Santos Online Gamemode
+
 Los Santos Online is a GTA V Roleplay server based on RAGE Multiplayer.
 This repository contains source files of server gamemode.
 
 **This gamemode is currently in development state, you shouldn't run it on production server.**
 
 ## Requirements
-1. SQL Server, preferred MariaDB
-2. Node.js installed on machine
+
+1.  SQL Server, preferred MariaDB
+2.  Node.js installed on machine
 
 ## Server setup
+
 First of all, you should download all of necessary dependencies. Run command described below to download them.
 
 ```bash
@@ -18,3 +21,11 @@ npm install
 # With Yarn
 yarn install
 ```
+
+## Building client files
+
+- `npm run hot`: runs `webpack-dev-server` with hot reload. Open `localhost:8080/dist/[view_name]/` to access
+
+- `npm run dev`: builds files for once with development mode
+
+- `npm run production`: will clear `dist/` as well `src/` :exclamation: directories & build files with minification
---
+++ client_packages/LSOnline/Browsers/src/pages/login/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="limiter">
+    <div class="container-login100">
+      <div class="login100-more">
+        <div class="container"  style="float:left;margin-top: 50px;font-size: 20px;padding:80px;min-width:100%;background-color: rgba(0,0,0,0.5)">
+          <h1>Witaj na serwerze <b>Los Santos Online</b>!</h1>
+          <br/>
+          Skorzystaj z panelu do logowania po prawej stronie aby się zalogować na konto, które utworzyłeś na naszym
+          forum. Jeśli nie posiadasz konta na naszym forum to wejdź na nie korzystając z adresu <i>lsonline.pl</i>
+          a następnie zarejestruj się oraz jako nowy użytkownik dodaj swoją pierwszą postać.
+        </div>
+      </div>
+      <div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
+        <form class="login100-form validate-form" @submit="submitForm">
+          <span class="login100-form-title p-b-59">
+          Zaloguj się
+          </span>
+          <div class="wrap-input100 validate-input" v-bind:class="{ 'alert-validate': errors['username'] }" data-validate="Username is required" @input="errors['username'] = false">
+            <span class="label-input100">Username</span>
+            <input class="input100" type="text" name="username" placeholder="Username..." v-model.trim="username">
+            <span class="focus-input100"></span>
+          </div>
+          <div class="wrap-input100 validate-input" v-bind:class="{ 'alert-validate': errors['password'] }" data-validate = "Password is required" @input="errors['password'] = false">
+            <span class="label-input100">Password</span>
+            <input class="input100" type="password" id="pass" name="pass" placeholder="*************" v-model.trim="password">
+            <span class="focus-input100"></span>
+          </div>
+          <div class="container-login100-form-btn">
+            <div class="wrap-login100-form-btn">
+              <div class="login100-form-bgbtn"></div>
+              <button class="login100-form-btn" @click="signIn">
+                Zaloguj się
+              </button>
+            </div>
+            <a href="#" class="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
+              Wyjdź z gry
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "notificationParent",
+  data() {
+    return {
+      errors: { username: false, password: false },
+      username: "",
+      password: ""
+    }
+  },
+  methods: {
+    signIn() {
+      mp.trigger('loginButtonClicked', this.password);
+    },
+    submitForm(e) {
+      if(!this.username) this.errors.username = true;
+      if(!this.password) this.errors.password = true;
+      
+      e.preventDefault();
+    },
+  },
+  components: {
+    FontAwesomeIcon
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../../assets/scss/style.scss";
+
+.login100-more {
+  background-image: url("../../assets/images/bg-01.jpg");
+}
+</style>
