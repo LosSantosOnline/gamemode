86e3b417442ad3f093f1e8069a2dec386cd50c95
kubeeek <jakub.galecki5@gmail.com>
2018-07-15 22:30:20 +0200

    reworked Login view

--- client_packages/LSOnline/Login/Login.js
+++ client_packages/LSOnline/Login/Login.js
@@ -2,25 +2,32 @@
 
 const browser = require("/LSOnline/Util/Browser");
 const camera = require("/LSOnline/Util/Camera");
-
+const Overlay = require("/LSOnline/Util/Overlay");
 function preparePanel(url) {
-    browser.prepareScreen();
-    camera.createCamera(3223, 5349, 14, 0, 0, 218, 20);
-    browser.open(url);
+  browser.prepareScreen(1000);
+  camera.createCamera(3223, 5349, 14, 0, 0, 218, 20);
+  browser.open(url);
 }
 
 function destroyPanel() {
-    camera.destroyCamera();
-    browser.close();
+  camera.destroyCamera();
+  browser.close();
 }
 
 mp.events.add({
-    'loginPanelAppeared': (url) => {
-        preparePanel(url);
-    },
-    'loginButtonClicked': (password) => {
-        destroyPanel();
-        mp.players.local.setInvincible(true);
-        mp.events.callRemote("loginPlayer", password);
-    }
+  loginPanelAppeared: url => {
+    preparePanel(url);
+    setTimeout(() => {
+      Overlay.notify(
+        "Nie posiadasz konta?",
+        "Wejdź na lsonline.pl i załóż je już teraz",
+        "info",
+        5000
+      );
+    }, 5000);
+  },
+  loginButtonClicked: password => {
+    mp.players.local.setInvincible(true);
+    mp.events.callRemote("loginPlayer", password);
+  }
 });
