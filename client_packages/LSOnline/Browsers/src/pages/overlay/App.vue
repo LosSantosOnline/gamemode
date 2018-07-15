a9e8f9befdedbf5f8c92a1fac230716dd0455bed
kubeeek <jakub.galecki5@gmail.com>
2018-07-15 19:56:26 +0200

    Reworked Notifications to Overlay

---
+++ client_packages/LSOnline/Browsers/src/pages/overlay/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <notification class="notification"></notification>
+</template>
+
+<script>
+import Notification from "./components/Notification.vue";
+
+export default {
+  name: "notificationParent",
+  data() {
+    return {
+      notification: true,
+      title: "",
+      content: ""
+    };
+  },
+  methods: {
+    showNotification(title, content, timeout = 1000) {
+      this.title = title;
+      this.content = content;
+      this.notification = true;
+    },
+    hideNotification() {
+      this.notification = false;
+    }
+  },
+  components: {
+    Notification
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/scss/transitions.scss";
+@import url("https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700");
+html,
+body {
+  overflow: hidden;
+}
+.notification {
+  position: absolute;
+  right: 0px;
+  width: 30%;
+}
+</style>
