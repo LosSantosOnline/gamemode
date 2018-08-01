4784d6b7f33af484bc09076a2d865fca20337daa
kubeeek <jakub.galecki5@gmail.com>
2018-08-01 14:12:43 +0200

    models are now loaded dynamically from the directory

--- packages/LSOnline/modules/database/Database.js
+++ packages/LSOnline/modules/database/Database.js
@@ -1,41 +1,53 @@
 // Libs
-const Sequelize = require('sequelize');
-const Logger = require('../utils/Logger');
-const db = {};
+const Sequelize = require('sequelize')
+const Logger = require('../utils/Logger')
+const glob = require('glob')
+const path = require('path')
+const db = {}
 
 // Create connection
 const connection = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-    host: process.env.DATABASE_HOST,
-    dialect: 'mysql',
-    operatorsAliases: false,
-    
-    //logging: false,
+  host: process.env.DATABASE_HOST,
+  dialect: 'mysql',
+  operatorsAliases: false,
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
+  // logging: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+})
 
 // Test database connection
 connection
-    .authenticate()
-    .then(() => {
-        Logger.info(`[Database] Connection has been established successfully. Connected to '${process.env.DATABASE_HOST}' database.`)
-    })
-    .catch(err => {
-        Logger.fatal(`[Database] Unable to connect to the database! (Error: ${err})`)
-    });
-
-// Assign connection to 'db'
-db.connection = connection;
-db.Sequelize = Sequelize;
+  .authenticate()
+  .then(() => {
+    Logger.info(`[Database] Connection has been established successfully. Connected to '${process.env.DATABASE_HOST}' database.`)
+  })
+  .catch(err => {
+    Logger.fatal(`[Database] Unable to connect to the database! (Error: ${err})`)
+  })
 
 // Models
-db.character = require('../../models/Character')(connection, Sequelize);
-db.vehicle = require('../../models/Vehicle')(connection, Sequelize);
+glob.sync('./packages/LSOnline/models/*.js').forEach((file) => {
+  file = path.parse(file)
+  if (file.name !== 'Account') {
+    db[file.name.toLowerCase()] = connection['import'](path.join(__dirname, '../../models', file.name))
+  }
+})
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db)
+  }
+})
+
+// Assign connection to 'db'
+db.connection = connection
+db.Sequelize = Sequelize
 
 // Export module
-module.exports = db;
+module.exports = db
