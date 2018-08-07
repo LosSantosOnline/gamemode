16f9380630d37a1ad2d56c7c9aab5f7118ca2371
kubeeek <jakub.galecki5@gmail.com>
2018-08-07 23:57:26 +0200

    pushed wrong file. Now fix.

--- packages/LSOnline/modules/auth/commands/AuthorizePlayerCommand.js
+++ packages/LSOnline/modules/auth/commands/AuthorizePlayerCommand.js
@@ -6,7 +6,12 @@ const logger = require('../../auth/AuthorizationLogger');
 const characterManager = require('../../characters/CharacterManager');
 
 exports.execute = async (player, login, password) => {
-  const chars = await characterManager.findByAccountId(1);
-  player.call('userAuthorized', [JSON.stringify(chars)]);
-}
-;
+  await service.ipbAuth(login, password).then(() => {
+    accountManager.loadAccountData(player, login).then(async (player) => {
+      const chars = await characterManager.findByAccountId(player.account.id);
+      player.call('userAuthorized', [JSON.stringify(chars)]);
+    });
+  }, (err) => {
+    logger.error(err);
+  });
+};
