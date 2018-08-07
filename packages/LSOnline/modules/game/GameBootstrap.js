8deaa92e2cdf0b7104afc6db98bb31baecb8f88c
kubeeek <jakub.galecki5@gmail.com>
2018-08-07 23:48:43 +0200

    Added some logging

--- packages/LSOnline/modules/game/GameBootstrap.js
+++ packages/LSOnline/modules/game/GameBootstrap.js
@@ -1,5 +1,7 @@
-"use strict";
+'use strict';
 
-const vehicleBootstrap = require("../vehicles/VehicleBootstrap");
+const vehicleBootstrap = require('../vehicles/VehicleBootstrap');
+
+console.log('\x1b[36m%s\x1b[0m', '[Server] Bootstraping game...');
 
 vehicleBootstrap.boot();
